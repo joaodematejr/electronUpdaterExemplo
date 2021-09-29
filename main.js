892ffc1bdc02ceec7fc950577a992859e2d74b2c
João Dematé Jr <joaodematejr@gmail.com>
2021-09-29 11:47:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -95,6 +95,10 @@ app.whenReady().then(() => {
   })
 })
 
+app.on('ready', async () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
